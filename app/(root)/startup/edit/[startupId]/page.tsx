042c70048b742e98598f9f1502112e16f8bc2809
Hamed-Ajaj <hamedsgaming@gmail.com>
2025-06-17 14:35:38 +0300

    adding authorization rules

--- app/(root)/startup/edit/[startupId]/page.tsx
+++ app/(root)/startup/edit/[startupId]/page.tsx
@@ -1,11 +1,17 @@
+import { auth } from "@/auth";
 import StartupForm from "@/components/startup-form";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { redirect } from "next/navigation";
 
 async function EditStartup({ params }: { params: Promise<{ startupId: string }> }) {
+  const session = await auth();
   const { startupId } = await params;
   const post = await client.fetch(STARTUP_BY_ID_QUERY, { postId: startupId })
-  console.log(post)
+
+  if (!session || session.id !== post?.author._id) {
+    redirect("/")
+  }
   return (
     <div>
       <section className="pink_container !min-h-[230px] pattern">
